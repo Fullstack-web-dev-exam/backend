8415458b63e627ba71015aa780b6c3aaf641ad27
Thomas <thomas@pop-os.localdomain>
2021-04-28 15:50:56 +0200

    Changed samesite to lax

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -116,7 +116,7 @@ function setTokenCookie(res, token) {
     httpOnly: true,
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     secure: true,
-    sameSite: 'none',
+    sameSite: 'lax',
   };
   res.cookie('refreshToken', token, cookieOptions);
 }
